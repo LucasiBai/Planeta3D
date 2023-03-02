85454342bd99d1d96b55d9b83028f6bd5a5429ee
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-03-02 18:07:07 -0300

    creacion de servicio de contacto wsp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
-import ContactPage from "./pages/ContactPage/ContactPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
-				<Route path="/contact" element={<ContactPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,21 +1,7 @@
-import ContactService from "../../services/ContactService";
-
 import PageTitle from "../../components/PageTitle/PageTitle";
-
 import "./ContactPage.css";
 
 const ContactPage = () => {
-	const sendMessage = () => {
-		const data = {
-			name: "",
-			phone: "holi",
-			email: "",
-			content: "dasd",
-		};
-
-		ContactService.sendMessage(data);
-	};
-
 	return (
 		<main className="contact-page__box">
 			<PageTitle>Contacto</PageTitle>
@@ -24,7 +10,6 @@ const ContactPage = () => {
 					<p>
 						¡Contame <span>tu idea</span> para que juntos la hagamos realidad!
 					</p>
-					<button onClick={sendMessage}>Send</button>
 				</article>
 			</section>
 		</main>
