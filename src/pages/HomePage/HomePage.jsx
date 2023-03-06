dd67522f26c9e3b9d783049e82da26c3d36e1294
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-03-06 18:51:08 -0300

    creacion de rainbowtext + maquetacion de home + actualizado de imagen de telescopiop

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,26 @@
+import ShadowButton from "../../components/ShadowButton/ShadowButton";
+import Button from "../../components/Button/Button";
+
+import "./HomePage.css";
+import RainbowText from "../../components/RainbowText/RainbowText";
+
 const HomePage = () => {
 	return (
-		<main>
-			<h1>Planeta 3D</h1>
+		<main className="home-page__box">
+			<section>
+				<article className="home-page__decoration"></article>
+				<article className="home-page__content">
+					<h2>
+						Transformá tus <RainbowText size={100}>ideas</RainbowText> en
+						realidad
+					</h2>
+					<div>
+						<ShadowButton>Nuestros Servicios</ShadowButton>
+						<Button>Escribime</Button>
+					</div>
+				</article>
+				<article className="home-page__decoration"></article>
+			</section>
 		</main>
 	);
 };
