18dadde95dabbe1ad550b84c9107b7abd803aa9f
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-03-09 13:22:04 -0300

    creacion de BrightImage + ajustes de accesibilidad en popchildbutton + estilos de AboutPage terminados

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,3 +1,4 @@
+import BrightImage from "../../components/BrightImage/BrightImage";
 import Timeline from "../../components/Timeline/Timeline";
 
 import "./AboutUsPage.css";
@@ -25,7 +26,27 @@ const AboutUsPage = () => {
 		<main className="about-us-page">
 			<section>
 				<Timeline timelineLabels={timelineLabels} />
-				<article></article>
+				<article className="text-block">
+					<BrightImage
+						src="https://www.denofgeek.com/wp-content/uploads/2021/10/Jim-The-Office-John-Krasinski.jpg?resize=768%2C432"
+						alt="daniel-testa"
+					/>
+					<p>
+						<span>
+							Soy Daniel Agustín Testa, Tengo 26 años, actualmente me dedico a
+							tiempo completo a mi emprendimiento de impresiones 3D.
+						</span>
+						<span>
+							La idea de mi emprendimiento surgió hace dos años, en el momento
+							que empecé a investigar sobre cómo era este mundo de la impresión
+							3D. Me informé, pedí asesoramiento por parte de profesionales para
+							la compra de mi primera impresora. Luego cuando la adquirí me fui
+							introduciendo en el tema aún más, haciendo capacitaciones, tomando
+							cursos que me sirvieron para adquirir profesionalismo a la hora de
+							producir y configurar las piezas a imprimir.
+						</span>
+					</p>
+				</article>
 			</section>
 		</main>
 	);
