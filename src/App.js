ba5ea6326a7c7b7a3a36cf3d5bbc06c899a91f52
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-03-10 15:39:52 -0300

    mejora de seo de todas las páginas de la app

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import AboutUsPage from "./pages/AboutUsPage/AboutUsPage";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import PopButtons from "./components/PopButtons/PopButtons";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<NavBar />
 			<PopButtons />
+			<h1 className="clipped">Planeta 3D</h1>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/about-us" element={<AboutUsPage />} />
-				<Route path="/services" element={<HomePage />} />
+				<Route path="/services" element={<ProductsPage />} />
 				<Route path="/help" element={<QuestionsPage />} />
 				<Route path="/contact" element={<ContactPage />} />
 				<Route path="*" element={<NotFoundPage />} />
--- src/pages/QuestionsPage/QuestionsPage.jsx
+++ src/pages/QuestionsPage/QuestionsPage.jsx
@@ -2,6 +2,7 @@ import DataService from "../../services/DataService";
 
 import { useData } from "../../hooks/useData";
 
+import { Helmet } from "react-helmet";
 import ExpandexLabelList from "../../components/ExpandexLabelList/ExpandexLabelList";
 import PageTitle from "../../components/PageTitle/PageTitle";
 
@@ -11,15 +12,24 @@ const QuestionsPage = () => {
 	const [qaList] = useData(DataService.getQuestions);
 
 	return (
-		<main className="questions-page__box">
-			<PageTitle>Preguntas Frecuentes</PageTitle>
-			<section>
-				<ExpandexLabelList qaList={qaList} />
-				<article className="image-decoration">
-					<img src="./assets/telescopio.png" alt="telescopio" />
-				</article>
-			</section>
-		</main>
+		<>
+			<Helmet>
+				<title>Preguntas Frecuentes | Planeta 3D</title>
+				<meta
+					name="description"
+					content="Encuentra aquí las respuestas a preguntas frecuentes sobre nuestros servicios de impresión en Planeta 3D. Conoce los métodos de pago, cuáles son los materiales disponibles y más. Obtén toda la información necesaria para llevar a cabo tus proyectos en 3D."
+				></meta>
+			</Helmet>
+			<main className="questions-page__box">
+				<PageTitle>Preguntas Frecuentes</PageTitle>
+				<section>
+					<ExpandexLabelList qaList={qaList} />
+					<article className="image-decoration">
+						<img src="./assets/telescopio.png" alt="telescopio" />
+					</article>
+				</section>
+			</main>
+		</>
 	);
 };
 
