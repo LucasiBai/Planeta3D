389a61de088f0ad75d65f6294eb33b992ba48480
tobiasmesa <tobiasmesa030303@gmail.com>
2023-03-12 18:25:49 -0300

    Agregando estilos a las cards

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet";
 import CardList from "../../components/CardList/CardList.jsx"
+import PageTitle from "../../components/PageTitle/PageTitle.jsx";
 import "./ProductsPage.css"
 
 const ProductsPage = () => {
@@ -12,7 +13,14 @@ const ProductsPage = () => {
 					content="En Planeta 3D ofrecemos servicios de impresión 3D de alta calidad para hacer realidad tus ideas y proyectos. Contamos con una amplia variedad de materiales y ofrecemos diferentes opciones de acabado para adaptarnos a tus necesidades. ¡Haz realidad tus ideas con nosotros!"
 				></meta>
 			</Helmet>
-			<main className="products-page">Products
+			<main className="products-page">
+				<div className="container-title">
+				<PageTitle subtitle={"Productos en stock"}>Impresiones 3d</PageTitle>
+				<article className="image-decoration">
+						<img src="./assets/saturno.png" className="img-saturno" alt="telescopio" />
+					</article>
+				</div>
+			
 			<CardList className="card-list"/>
 			</main>
 		</>
