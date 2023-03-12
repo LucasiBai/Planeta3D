6ff555fad7dd99cd4f6d4784bacd60fe595eb6d0
tobiasmesa <tobiasmesa030303@gmail.com>
2023-03-11 21:23:07 -0300

    Finalización de la navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import  {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTiktok, faInstagram } from "@fortawesome/free-brands-svg-icons";
-import './NavBar.css';
+import "./NavBar.css";
 
 function NavBar() {
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState("");
 
   const handleNavItemClick = (item) => {
     setActive(item);
@@ -13,37 +13,45 @@ function NavBar() {
 
   return (
     <nav>
-    <Link to="/" className="logo" onClick={() => handleNavItemClick('')}>
-      <img src="assets/logosinastro.svg" alt="Logo" />
-    </Link>
-    <ul className="nav-links">
-      <li className= {active === 'about-us' ? 'active' : ''} >
-        <Link to="/about-us" onClick={() => handleNavItemClick('about-us')}>
-          ¿Quienes somos?
-        </Link>
-      </li>
-      <li className={active === 'services' ? 'active' : ''}>
-        <Link to="/services" onClick={() => handleNavItemClick('services')}>
-          Nuestros servicios
-        </Link>
-      </li>
-      <li className={active === 'help' ? 'active' : ''}>
-        <Link to="help" onClick={() => handleNavItemClick('help')}>
-          Preguntas frecuentes
-        </Link>
-      </li>
-      <li className={active === 'contact' ? 'active' : ''}>
-        <Link to="/contact" onClick={() => handleNavItemClick('contact')}>
-          Contacto
-        </Link>
-      </li>
-    </ul>
-    <ul className='nav-logos'>   
-    <FontAwesomeIcon icon={faTiktok} className='logo-insta'/>
-    <FontAwesomeIcon icon={faInstagram} className='logo-tiktok'/>
-    </ul>
-  </nav>
+      <Link to="/" className="logo" onClick={() => handleNavItemClick("")}>
+        <img src="assets/logosinastro.svg" alt="Logo" />
+      </Link>
+      <ul className="nav-links">
+        <li className={active === "about-us" ? "active" : ""}>
+          <Link to="/about-us" onClick={() => handleNavItemClick("about-us")}>
+            ¿Quienes somos?
+          </Link>
+        </li>
+        <li className={active === "services" ? "active" : ""}>
+          <Link to="/services" onClick={() => handleNavItemClick("services")}>
+            Nuestros servicios
+          </Link>
+        </li>
+        <li className={active === "help" ? "active" : ""}>
+          <Link to="help" onClick={() => handleNavItemClick("help")}>
+            Preguntas frecuentes
+          </Link>
+        </li>
+        <li className={active === "contact" ? "active" : ""}>
+          <Link to="/contact" onClick={() => handleNavItemClick("contact")}>
+            Contacto
+          </Link>
+        </li>
+      </ul>
+      <ul className="nav-logos">
+        <li className="log-insta">
+          <a href="https://instagram.com/planeta3d__">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+        </li>
+        <li className="logo-tiktok">
+          <a href="https://tiktok.com/@planeta.3d">
+            <FontAwesomeIcon icon={faTiktok} size="2x" />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
-export default NavBar
+export default NavBar;
