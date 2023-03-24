e2505c3dff9e629a637ee56bba4223fe34a1d29d
tobiasmesa <tobiasmesa030303@gmail.com>
2023-03-24 19:24:03 -0300

    Agregandole reusabilidad a Cardlist, modificación de ProductsPage

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,41 +1,11 @@
 import CardItem  from '../CardItem/CardItem.jsx' 
 import "./CardList.css"
 
-const CardList = () => {
-    const cardData = [
-      {
-        title: 'Llavero',
-        subtitle: 'Cámara de fotos',
-        desc1: "Material: PETG ",
-        desc2: "Medidas: 3cm de alto por 5cm de ancho.",
-        image: '/assets/llavero-camara.png',
-      },
-      {
-        title: 'Juego sumergible',
-        subtitle: 'Estrella',
-        desc1: "Material: PLA",
-        desc2: "Medidas: 7cn de alto por 7cm de ancho.",
-        image: '/assets/juguete.png',
-      },
-      {
-        title: 'Llavero',
-        subtitle: 'Me gusta',
-        desc1: "Material: PETG",
-        desc2: "Medidas: 3cm de alto por 5cm de ancho.",
-        image: '/assets/llavero-mg.png',
-      },      
-      {
-        title: 'Botón',
-        subtitle: 'Smile',
-        desc1: "Material: PETG ",
-        desc2: "Medidas: 0,5 cm de alto por 0,5 cm de ancho.",
-        image: '/assets/botones.png',
-      },
-    ];
-  
+const CardList = (props) => {
+  const { cards } = props;
     return (
       <div className="card-list">
-        {cardData.map((card, index) => (
+        {cards.map((card, index) => (
           <CardItem
             key={index}
             title={card.title}
--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -3,6 +3,68 @@ import CardList from "../../components/CardList/CardList.jsx"
 import PageTitle from "../../components/PageTitle/PageTitle.jsx";
 import "./ProductsPage.css"
 
+const cardData = [
+	{
+	  title: 'Llavero',
+	  subtitle: 'Cámara de fotos',
+	  desc1: "Material: PETG ",
+	  desc2: "Medidas: 3cm de alto por 5cm de ancho.",
+	  image: '/assets/llavero-camara.png',
+	},
+	{
+	  title: 'Juego sumergible',
+	  subtitle: 'Estrella',
+	  desc1: "Material: PLA",
+	  desc2: "Medidas: 7cm de alto por 7cm de ancho.",
+	  image: '/assets/juguete.png',
+	},
+	{
+	  title: 'Llavero',
+	  subtitle: 'Me gusta',
+	  desc1: "Material: PETG",
+	  desc2: "Medidas: 3cm de alto por 5cm de ancho.",
+	  image: '/assets/llavero-mg.png',
+	},      
+	{
+	  title: 'Botón',
+	  subtitle: 'Smile',
+	  desc1: "Material: PETG ",
+	  desc2: "Medidas: 0,5 cm de alto por 0,5 cm de ancho.",
+	  image: '/assets/botones.png',
+	},
+  ];
+
+  const clienteData = [
+	{
+	  title: 'Rodillo',
+	  subtitle: 'Texturizados',
+	  desc1: "Material: PLA",
+	  desc2: "Medidas: 5 cm de alto por 6 cm de ancho.",
+	  image: '/assets/rodillos.png',
+	},
+	{
+	  title: 'Trofeo',
+	  subtitle: 'Torneo Comercial de Voley',
+	  desc1: "Material: PLA",
+	  desc2: "Medidas: 30 cm de alto por 15 cm de ancho.",
+	  image: '/assets/trofeo.png',
+	},
+	{
+	  title: 'Placa mascota',
+	  subtitle: 'Perro Loba',
+	  desc1: "Material: PLA",
+	  desc2: "Medidas: 2 cm de alto por 5 cm de ancho.",
+	  image: '/assets/loba.png',
+	},      
+	{
+	  title: 'Adorno',
+	  subtitle: 'Letra G',
+	  desc1: "Material: PLA ",
+	  desc2: "Medidas: 10 cm de alto por 10 cm de ancho.",
+	  image: '/assets/letrag.png',
+	},
+  ];
+
 const ProductsPage = () => {
 	return (
 		<>
@@ -20,9 +82,17 @@ const ProductsPage = () => {
 						<img src="./assets/saturno.png" className="img-saturno" alt="telescopio" />
 					</article>
 				</div>
-			
-			<CardList className="card-list"/>
+			<CardList cards={cardData} className="card-list"/>
 			</main>
+			<section>
+			<div className="container-title">
+				<PageTitle subtitle={"Lo que nuestros clientes digan"}>Diseños personalizados</PageTitle>
+				<article className="image-decoration">
+						<img src="./assets/saturno.png" className="img-saturno" alt="telescopio" />
+				</article>
+				</div>
+				<CardList cards={clienteData} className="card-list"/>
+			</section>
 		</>
 	);
 };
